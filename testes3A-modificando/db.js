d9c2680087d8b0a624dac0126faa0e60d5d9100d
Marco <162701191+Marc0Antonio@users.noreply.github.com>
2025-04-29 10:15:20 -0300

    a

--- testes3A-modificando/db.js
+++ testes3A-modificando/db.js
@@ -1,10 +1,9 @@
 async function connect() {
     const { Pool } = require("pg");
 
-     // Quando eu chamar a função connect novamente, verificarei se já tenho uma global.connection carregada. Se houver, simplesmente a retornarei.
-     // Essa estratégia é chamada de singleton. Ela impede que você recrie objetos completamente o tempo todo.
-    if(global.connection)
-        return global.connection.connect();
+    // Quando eu chamar a função connect novamente, verificarei se já tenho uma global.connection carregada. Se houver, simplesmente a retornarei.
+    // Essa estratégia é chamada de singleton. Ela impede que você recrie objetos completamente o tempo todo.
+    if (global.connection) return global.connection.connect();
 
     // Connexão "Pool" é uma estratégia de conexão onde o banco de dados abre algumas conexões e sempre que a gente precisa de uma nova conexão o banco pega nova conexão 
     // no Pool de conexões já abertos. Se tiver conexão ociosa, é essa que será entregue. Se tiver que criar uma nova conexão, a conexão é criada e se tiver que fechar 
@@ -16,29 +15,21 @@ async function connect() {
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         dialect: process.env.DB_DIALECT,
-        port: process.env.PORT_NUMBER
-    })
-    
+        port: process.env.PORT_NUMBER,
+    });
 
     const client = await pool.connect(); // Conectandooo
-    console.log("Connection pool created successfully!")
+    console.log("Connection pool created successfully!");
 
     const resdb = await client.query("SELECT now()");
     console.log(resdb.rows[0]); // Tomando a primeira posição do array de onde virá o tempo do banco de dados.
-    client.release()
+    client.release();
 
-  // Podemos salvar nosso pool em uma conexão global. Então podemos executar o "if" como no início deste arquivo
+    // Podemos salvar nosso pool em uma conexão global. Então podemos executar o "if" como no início deste arquivo
     global.connection = pool;
 
-    return pool.connect()
+    return pool.connect();
 }
 
 connect(); // Lembrando/Lembrete: temos que carregar/importar o arquivo db.js no nosso back-end index.js.
 
-// module.exports = {
-//     selectCustomers,
-//     selectCustomer,
-//     insertCustomer,
-//     updateCustomer,
-//     deleteCustomer
-// }
